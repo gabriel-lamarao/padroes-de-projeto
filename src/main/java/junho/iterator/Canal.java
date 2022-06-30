8ac9861c7c2160fef4bf1439f582806c83b9d26a
Gabriel Lamarao <gabriel.lamarao97@gmail.com>
2022-06-30 12:10:12 -0300

    4 em 1

---
+++ src/main/java/junho/iterator/Canal.java
@@ -0,0 +1,9 @@
+package junho.iterator;
+
+public class Canal {
+	String nome;
+
+	public Canal(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ src/main/java/junho/mediator/Mediator.java
@@ -0,0 +1,7 @@
+package junho.mediator;
+
+public interface Mediator {
+
+	void enviar(String mensagem, Colleague colleague);
+
+}
